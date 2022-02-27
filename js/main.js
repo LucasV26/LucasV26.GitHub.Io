dbab8850796ab80029dd706fc0bd2df5f2c90058
Lucas Vinicius Sousa Silva <lucas2606.vinicius@gmail.com>
2022-02-27 17:59:49 -0300

    Altertando estilo do portifólio

--- js/main.js
+++ js/main.js
@@ -1,61 +1,31 @@
-//Lidando com a atualização automática de idade e tempo decorrido
-let campo_idade = document.querySelector('span#idade');
-let campo_anos = document.querySelector('span#anos');
-let ano_atual = new Date();
-ano_atual = ano_atual.getFullYear();
+function init() {
+    const descricao = document.getElementById("descricao");
+    att_descricao(descricao);
 
-campo_idade.innerHTML = (ano_atual - 2001);
-campo_anos.innerHTML = (ano_atual - 2019);
-
-campo_anos.innerHTML==1? campo_anos.innerHTML += ' year' : campo_anos.innerHTML += ' years';
-// Fim da área de idade e tempo decorrido 
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
-//Trabalhando com animações usando EventListeners
-let campo_cabecalho = document.querySelector('div#cabecalho');
-let imagem_logo = document.querySelector('img#logo');
-
-campo_cabecalho.addEventListener('mouseenter', function(){
-    imagem_logo.style.objectPosition = "initial"; //Propriedades CSS que usam hífem (-) são escritas em camelCase em JS
-})
-
-campo_cabecalho.addEventListener('mouseleave', function(){
-    imagem_logo.style.objectPosition = "-200px";
-})
-
-////
-
-let campo_eu = document.querySelector('figure#eu');
-let imagem_moldura = document.querySelector('img#moldura');
-
-campo_eu.addEventListener('mouseenter', function(){
-    imagem_moldura.style.backgroundImage = "url('imagem/retrato2.jpg')";
-})
-
-campo_eu.addEventListener('mouseleave', function(){
-    imagem_moldura.style.backgroundImage = "url('imagem/retrato1.jpg')";
-
-})
-
-////
-
-let elemento_lista = document.getElementsByClassName('lista');
-let elemento_linkD = document.getElementsByClassName('linkD');
-
-for(let index in elemento_lista) {
-    elemento_lista[index].addEventListener('mouseenter', function(){
-        elemento_lista[index].style.backgroundColor = 'black';
-        elemento_lista[index].style.paddingTop = '20px';
-        elemento_lista[index].style.transition = '1s';
-        elemento_linkD[index].style.color = 'white';
-    })
-
-    elemento_lista[index].addEventListener('mouseleave', function(){
-        elemento_lista[index].style.backgroundColor = 'gray';
-        elemento_lista[index].style.paddingTop = '2px';
-        elemento_lista[index].style.transition = '1s';
-        elemento_linkD[index].style.color = 'black';
-    })
+    const projetos_flex = document.getElementById("projects-flex");
+    rend_projetos(projetos_flex);
 }
-//Fim da área de animações
+
+function att_descricao(elemento) {
+    const ano_atual = new Date().getFullYear();
+    elemento.innerHTML = `Tenho estudado Desenvolvimento de Sistemas pelos últimos ${ano_atual - 2019} anos, desenvolvendo alguns projetos pessoais e acadêmicos, no processo.<br/>Adquiri experiência com Desenvolvimento Web, APIs REST, Banco de Dados, Lógica de Programação, DevOps, dentre outras tecnologias.<br/>`;
+}
+
+function rend_projetos(elemento) {
+    saida = ``;
+    for(let i of projetos){
+        saida += `
+                <div class="projects-card">
+                    <h4> ${i.data} </h4>
+                    <h2> ${i.titulo} </h2>
+                    <p>  ${i.descricao} </p>
+                </div>
+        `
+    }
+
+    elemento.innerHTML = saida;
+}
+
+init();
+
+
---
+++ js/projetos.js
@@ -0,0 +1,17 @@
+const projetos = [
+    {
+        data: "Jan 2021 - Nov 2021",
+        titulo: "Re:Donate",
+        descricao: "Meu Projeto de Conclusão de Curso.<br/>Uma aplicação web desenvolvida para conectar pessoas e instituições que necessitam de dinheiro com pessoas que estão dispostas a efetuar doações beneficentes.<br/>Desenvolvido utilizando SpringBoot, SpringData JPA, MySQL, Thymeleaf, WebSocket e mais.<br/>Veja mais sobre no <a href='https://bit.ly/3EWiMQj' target='_blank'>repositório</a>"
+    },
+    {
+        data: "Out 2021 - Nov 2021",
+        titulo: "TFT API",
+        descricao: "Um estudo sobre APIs REST.<br/>Um sistema distribuído baseado nas mecânicas do jogo <a href='https://teamfighttactics.leagueoflegends.com/' target='_blank'>TeamFight Tactics</a>.<br/>Desenvolvido utilizando SpringBoot, SpringData JPA, MySQL, RestTemplate, JavaScrips, Jquery e mais.<br/>Veja mais no <a href='https://bit.ly/336Xdz7' target='_blank'>repositório</a>"
+    },
+    {
+        data: "Jun 2021 - Dias atuais",
+        titulo: "P5JS Pages",
+        descricao: "Meu projeto pessoal sobre <a href='https://p5js.org/' target='_blank'>P5JS</a>.<br/>Uma coleção de páginas de arte visual que utilizam matemática, física e lógica de programação para criar imagens, padrões e comportamentos.<br/>Desenvolvido utilizando HTML, CSS, JavaScript e P5JS.<br/>Visualize as artes <a href='https://bit.ly/3zvoQ13' target='_blank'>aqui</a> ou veja o código no <a href='https://github.com/LucasV26/P5JS-Pages' target='_blank'>repositório</a>"
+    }
+]
